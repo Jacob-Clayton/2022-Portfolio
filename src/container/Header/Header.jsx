b44815b81c471a91b2f2c9dfeba6e908e8df2bfe
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2022-12-17 02:50:03 +0000

    Working individual letter animation

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,7 +4,35 @@ import {BsChevronDoubleDown} from 'react-icons/bs'
 import { AppWrap } from '../../wrapper'
 import './Header.scss';
 
-const Header = () => {
+const Header = ({ text = 'Jacob Clayton' }) => {
+  const letters = Array.from(text);
+
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.2 * i },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      transition: {
+        type: "tween",
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: -5,
+      y: 25,
+      transition: {
+        type: "tween",
+      },
+    },
+  };
 
   return (
     <motion.section 
@@ -19,9 +47,19 @@ const Header = () => {
           Hi, my name is
         </span>
 
-        <h1 className='title'>
-          Jacob Clayton
-        </h1>
+        <motion.h1 
+        className='title'
+        style={{ overflow: "hidden" }}
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        >
+          {letters.map((letter, index) => (
+          <motion.span variants={child} key={index}>
+            {letter === " " ? "\u00A0" : letter}
+          </motion.span>
+        ))}
+        </motion.h1>
 
         <span className="subTitle">
           Data Analyst and Full-Stack Developer.
