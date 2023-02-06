1cd4052d04ea54c1f36d12a05c41afb343add033
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-06 02:28:52 +0000

    nothing

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -87,16 +87,15 @@ const Projects = () => {
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
-                    <AiFillGithub />
+                    <AiFillGithub/>
                   </motion.div>
                 </a>
               </motion.div>
             </div>
-
+            
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
-
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
