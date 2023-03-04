532d4becdd5a4417f51195713adda9815aa30435
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-04 18:05:15 +0000

    removed images and installed tailwind

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,110 +1,48 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import node from '../assets/node.png';
-import python from '../assets/python.png';
-import react from '../assets/react.png';
 import sass from '../assets/sass.png';
-import vue from '../assets/vue.png';
-
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
-import profile from '../assets/profile.png';
-import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
-
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
-
 import pythonlarge from '../assets/pythonlarge.png'
 import sqllarge from '../assets/sqllarge.png'
 import tableaularge from '../assets/tableaularge.png'
-import jclogosize from '../assets/jclogosize.png'
-import jclogosmall from '../assets/jclogosmall.png'
-import jclogo from '../assets/jclogo.png'
 import gmail from '../assets/gmail.png'
-import meta from '../assets/meta.png'
-import google from '../assets/google.png'
-import michigan from '../assets/michigan.png'
-import ucdavis from '../assets/ucdavis.png'
 import reacthd from '../assets/reacthd.png'
 import apicog from '../assets/apicog.png'
 import cloud from '../assets/cloud.png'
-import kolsaspfpwhite from '../assets/kolsaspfpwhite.png'
-import kolsaspfp from '../assets/kolsaspfp.png'
-
-import blob from '../assets/blob.svg'
 import linkedin from '../assets/linkedin.svg'
-import bgblue from '../assets/bgblue.png'
-import bgblue2 from '../assets/bgblue2.png'
-import ucdavisblue from '../assets/ucdavisblue.svg'
-import bloblb from '../assets/bloblb.svg'
 import jacobpfp from '../assets/jacobpfp.png'
 
+import bridgr from '../assets/bridgr.png'
+import dexdash from '../assets/dexdash.png'
+import discord from '../assets/discord.png'
+import ethanalysis from '../assets/ethanalysis.png'
+import gasdash from '../assets/gasdash.png'
+import portfolio from '../assets/portfolio.png'
+
 export default {
-  email,
-  mobile,
-  api,
-  cpp,
   css,
   figma,
-  flutter,
   git,
   html,
   javascript,
   node,
-  python,
-  react,
   sass,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
-  profile,
-  circle,
-  logo,
-  adidas,
-  amazon,
-  asus,
-  nb,
-  skype,
-  spotify,
   pythonlarge,
   sqllarge,
   tableaularge,
-  jclogosize,
-  jclogosmall,
-  blob,
-  gmail,
-  linkedin,
-  bgblue,
-  bgblue2,
-  meta,
-  google,
-  ucdavis,
-  michigan,
-  ucdavisblue,
   apicog,
+  linkedin,
+  gmail,
   reacthd,
-  jclogo,
   cloud,
-  bloblb,
   jacobpfp,
-  kolsaspfpwhite,
-  kolsaspfp,
+  bridgr,
+  dexdash,
+  discord,
+  ethanalysis,
+  gasdash,
+  portfolio,
 };
