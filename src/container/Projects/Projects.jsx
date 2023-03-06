dbc0bc2f06f98c098001dda7a04330e1e9689e94
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-06 16:53:17 +0000

    code and live always visible with text

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -65,7 +65,6 @@ const Projects = () => {
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
-                whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
@@ -73,21 +72,23 @@ const Projects = () => {
 
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.98] }}
-                    transition={{ duration: 0.25 }}
+                    whileHover={{ scale: [1, 0.97] }}
+                    transition={{ duration: 0.2 }}
                     className="app__flex"
                   >
                     <AiFillEye />
+                    <p>Live</p>
                   </motion.div>
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.98] }}
-                    transition={{ duration: 0.25 }}
+                    whileHover={{ scale: [1, 0.97] }}
+                    transition={{ duration: 0.2 }}
                     className="app__flex"
                   >
                     <AiFillGithub/>
+                    <p>Code</p>
                   </motion.div>
                 </a>
               </motion.div>
